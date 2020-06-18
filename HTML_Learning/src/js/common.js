f3a03cdb847f1c8bf3f4bb167e6da08ecd797aa5
binhtrandev <binhtrandev@gmail.com>
2020-06-18 20:36:21 +0700

    JS select box add option

--- HTML_Learning/src/js/common.js
+++ HTML_Learning/src/js/common.js
@@ -15,3 +15,14 @@ function nhan(){
 function onMouseLeaveDemo(){
 	alert("Your onmouse leave me!");
 }
+
+function loadSelectBox(id, arr){
+	var val = "";
+	for(var i = 0; i < arr.length; i ++){
+		val = arr[i];
+		var addedOption = document.createElement("option");
+        addedOption.text = val;
+        addedOption.value = val.toLowerCase();
+		document.getElementById(id).appendChild(addedOption);
+	}
+}
