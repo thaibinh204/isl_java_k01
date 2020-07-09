20ceca3f01ca217d952ee6fd3c3e521fb8861edf
binhtrandev <binhtrandev@gmail.com>
2020-07-09 19:27:16 +0700

    no message

--- Spring_Hai/SpringBootThymeleft/src/main/java/com/example/thymeleafdemo/config/WebSecurityConfig.java
+++ Spring_Hai/SpringBootThymeleft/src/main/java/com/example/thymeleafdemo/config/WebSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -54,6 +53,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 				.logout().permitAll()
 				.and()
-				.exceptionHandling();
+				.exceptionHandling().accessDeniedPage("/403");
     }
 }
